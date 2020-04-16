f55ac3a49638912f30f297164f38f11abc1f1c3c
Mohammad Ghorab <mody.moustafa@yahoo.com>
2020-04-16 20:40:52 +0200

    Tailwind configurations

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,6 +1,19 @@
+const brandColors = new Object({
+  'red': '#e80001'
+})
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand': brandColors
+      },
+      backgroundColor: {
+        'gray': '#fafafe'
+      },
+      borderColor: {
+        'gray': '#e7e7e7'
+      }
+    },
   },
   variants: {},
   plugins: [],
