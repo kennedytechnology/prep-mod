78bd30aec1910046d52315a03680b84359bc4872
Sam Kennedy <sam@samrolken.org>
2020-04-20 09:56:15 -0400

    production configuration for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,17 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
