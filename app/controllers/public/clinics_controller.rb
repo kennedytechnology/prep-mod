b15ff7d3afc8ebc7e6d7a78cf1f058e319c4bfda
Sam Kennedy <sam@samrolken.org>
2020-04-19 22:19:15 -0400

    Updates+fixes to clinic search

--- app/controllers/public/clinics_controller.rb
+++ app/controllers/public/clinics_controller.rb
@@ -10,12 +10,14 @@ class Public::ClinicsController < ApplicationController
       params[:search_radius] = 999 if params[:search_radius] == "All"
       @clinics = @clinics.near(params[:location], params[:search_radius].to_i, order: :distance)
     end
+    # TODO: Perform this with ActiveRecord + SQL and avoid premature conversion to array here.
     if params['age_group']
       @clinics = @clinics.select{|clinic| (clinic.age_groups.collect(&:name) & params['age_group']).any?  }
     end
     if params['service']
       @clinics = @clinics.select{|clinic| (clinic.services.collect(&:name) & params['service']).any?  }
     end
+    @clinics = @clinics.take(10)
     render :index   
   end
 
