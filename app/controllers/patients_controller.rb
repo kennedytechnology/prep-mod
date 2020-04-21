4ba9bfc3cdb802e61c803b7bed1e35d4f0a06b91
samrolken <sam@samrolken.org>
2020-04-21 05:29:45 -0400

    Revert "Patients controller patient params" (#18)

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -40,11 +40,7 @@ class PatientsController < ApplicationController
 private
 
 def patient_params
-  params.require(:patient).permit(:clinic, :clinic_id, :user_id, :student_id, :access_code, :vaccination_status, :clinic_vaccine_id, 
-                                  :clinic_staff_id, :reaction_type, :downloaded_status, :state, :county, :city, :zip_code, :school, 
-                                  :first_name, :last_name, :mothers_maiden_name, :middle_initial, :age, :address, :email, :email_confirmation,
-                                  :date_of_birth, :sex, :phone_number, :relation_to_patient_for_insurance, :insurance_type,
-                                  :insured_first_name, :insured_last_name, :insured_name, :insured_date_of_birth, :type_of_insurance,
-                                  :member_id_for_insurance, :card_number_for_insurance, :group_number_for_insurance,:insurance_company_name, 
-                                  :has_fever_over, :has_caugh, :has_difficult_breathing, :had_contact_with_confirmed_case, :is_age_60_or_more, :had_traveled_to_affected_place, :has_risk_factor, :has_other_reason, :other_reason_explanation, :consent_signature, :relation_to_patient_for_consent, :consent_date, :password, :password_confirmation, :notify_via_sms, :notify_via_email)
+  params.require(:patient).permit(:clinic, :clinic_id, :middle_initial, :address, :zip_code, :state, :county, :city, :school, :email, :phone_number, :first_name, :last_name, :mothers_maiden_name, :date_of_birth,  :password, :password_confirmation, :age, :sex, :notify_via_sms, :notify_via_email)
+end
+
 end
