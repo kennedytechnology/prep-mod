d1f07e0051652e6e7b230a70f2659ec6ecabc61a
samrolken <sam@samrolken.org>
2020-04-27 03:23:21 -0400

    Add search functionality to the clinic activity form (#58)

--- app/javascript/controllers/clinic_activity_patients_controller.js
+++ app/javascript/controllers/clinic_activity_patients_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "modal" ]
+  static targets = [ "modal", "searchableRow" ]
 
   showModal(event) {
     let modal_id = "clinic-activity-modal-" + event.target.dataset.selection + "-" + event.target.closest("tr").dataset.patientId
@@ -10,6 +10,14 @@ export default class extends Controller {
     modal.classList.toggle('pointer-events-none')
     // window.scrollTo(0, 0);
   }  
+
+  search(event) {
+    let searchTerm = event.target.value.toLowerCase()
+    this.searchableRowTargets.forEach((el, i) => {
+      let filterableKey =  el.getAttribute("data-search-content")
+      el.classList.toggle("hidden", !filterableKey.includes( searchTerm ) )
+    })
+  }
 
   connect() {
     
