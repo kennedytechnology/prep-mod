de413bc2ea12436d369f844bb98f7f3156ad3e8f
Orce <orce@kennedytech.mk>
2020-04-28 18:21:02 +0200

    Sorting of patients asc/desc

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,7 +4,7 @@ class PatientsController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @patients = Patient.all.paginate(page: params[:page], per_page: 50)
+    @patients = Patient.order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 50)
     if params[:clinic_id]
       @clinic = Clinic.find(params[:clinic_id])
       # @patients = @clinic.patients
