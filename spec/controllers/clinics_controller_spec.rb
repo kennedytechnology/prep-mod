aa2bb9327010169818ddf2b9647122ee57a0c093
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-03 15:42:44 +0200

    clinics spec permit params

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -74,4 +74,49 @@ RSpec.describe ClinicsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  it do
+    params = {
+      clinic: {
+        venue_name: Faker::University.unique.name,
+        clinic_date: Faker::Date.between(from: 1.month.ago, to: 6.months.from_now),
+        lead_vaccinator_name: Faker::Name.unique.name,
+        students_registered: Faker::Number.normal(mean: 100, standard_deviation: 50),
+        clinic_status: %w(Pending Completed Cancelled).sample,
+        outcome_comments: Faker::Lorem.paragraph(sentence_count: 0, random_sentences_to_add: 9, supplemental: true),
+        incidents_comments: Faker::Lorem.paragraph(sentence_count: 0, random_sentences_to_add: 9, supplemental: true),
+        start_time: "#{(9..12).to_a.sample}:00",
+        end_time: "#{(13..17).to_a.sample}:00",
+        duration: 180,
+        age_groups: ClinicAgeGroup.all.sample(rand(ClinicAgeGroup.count) + 1),
+        services: ClinicService.all.sample(rand(ClinicService.count) + 1),
+        address: "8117 Mountain View Circle, Pasadena, MD 21122",
+        longitude: -76.567383,
+        latitude: 39.133262,
+        appointment_frequency_minutes: [10, 15, 30, 60].sample,
+        appointment_slots: (2..10).to_a.sample,
+        appointments_available: 'required',
+        county: COUNTIES.sample
+      }
+    }
+    should permit(:clinic_status, :start_time, :end_time,
+      :address, :lead_vaccinator_name,
+      :clinic_date, :students_registered,
+      :incidents_comments, :county, :venue_name, :zip, 
+      :city, :state, :appointment_frequency_minutes,
+      :appointment_slots, :contact_person, :contact_phone_number,
+      :backup_contact_person, :backup_contact_phone_number,
+      :start_hour_minute, :start_meridiem,
+      :end_hour_minute, :end_meridiem,
+      :appointments_available, users: [], :service_ids => [],
+      :age_group_ids => [],
+      clinic_personnel_attributes: [:id, :name, :_destroy],
+      clinic_events_attributes: [:id, :patient_id, :outcome, :safety_kit_received],
+      test_kits_attributes: [:id, :test_name, :test_manufacturer,
+      :test_lot_number, :test_type, :test_processing, 
+      :test_expiration_date, :test_kits_quantity, :tests_administered, 
+      :unusable_tests, :tests_returned, :_destroy]).
+      for(:create, params: params).
+      on(:clinic)
+  end
 end
