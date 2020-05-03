83b3a370ef534fc8b60ed0fd8fddbc7b5dd08ed3
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-03 15:35:49 +0200

    clinics controllers create/update action specs

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -18,10 +18,53 @@ RSpec.describe ClinicsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "create new clinic" do
+        expect {
+          post :create, params: { clinic: {
+            venue_name: Faker::University.unique.name,
+            clinic_date: Faker::Date.between(from: 1.month.ago, to: 6.months.from_now),
+            lead_vaccinator_name: Faker::Name.unique.name,
+            students_registered: Faker::Number.normal(mean: 100, standard_deviation: 50),
+            clinic_status: %w(Pending Completed Cancelled).sample,
+            outcome_comments: Faker::Lorem.paragraph(sentence_count: 0, random_sentences_to_add: 9, supplemental: true),
+            incidents_comments: Faker::Lorem.paragraph(sentence_count: 0, random_sentences_to_add: 9, supplemental: true),
+            start_time: "#{(9..12).to_a.sample}:00",
+            end_time: "#{(13..17).to_a.sample}:00",
+            duration: 180,
+            age_groups: ClinicAgeGroup.all.sample(rand(ClinicAgeGroup.count) + 1),
+            services: ClinicService.all.sample(rand(ClinicService.count) + 1),
+            address: "8117 Mountain View Circle, Pasadena, MD 21122",
+            longitude: -76.567383,
+            latitude: 39.133262,
+            appointment_frequency_minutes: [10, 15, 30, 60].sample,
+            appointment_slots: (2..10).to_a.sample,
+            appointments_available: 'required',
+            county: COUNTIES.sample
+          } }
+        }.to change(Clinic, :count).by(1)
+      end
+    end
+  end
+
   describe "GET #edit" do
     it "returns http success" do
       get :edit, params: { id: clinic.id }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #update" do
+    context "success" do
+      before do
+        put :update, params: { id: clinic.id, clinic: { clinic_date: Date.today } }
+        clinic.reload
+      end
+
+      it { expect(response).to redirect_to root_path }
+      it { expect(flash[:notice]).to match("Success. Clinic was updated successfully!") }
+      it { expect(clinic.clinic_date).to eq(Date.today) }
     end
   end
 
