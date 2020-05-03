0a72656dc0c953dc4c4b1d1428df1f7f21f8d3da
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-03 17:47:19 +0200

    Mailers specs

--- spec/mailers/news_signup_spec.rb
+++ spec/mailers/news_signup_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe NewsSignupMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Successful Signup" do
+    let(:news_signup) { create(:news_signup) }
+    let(:mail) { NewsSignupMailer.successful_signup(news_signup) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Thank you for your subscription")
+      expect(mail.to).to eq([news_signup.email])
+      expect(mail.from).to eq(["info@clinicwizard.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match(news_signup.email)
+    end
+  end
 end
