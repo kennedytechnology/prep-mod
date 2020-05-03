df02ea9d97f202fe8fe04c67b52574e656fb9c2f
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-03 13:51:27 +0200

    News Signup specs

--- spec/factories/news_signups.rb
+++ spec/factories/news_signups.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :news_signup do
-    name { "MyString" }
-    email { "MyString" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    date_of_birth { Faker::Date.birthday }
+    zip_code { Faker::Address.zip_code }
+    topics { ["Testing", "Screening", "Safety Kit", "Medication", "Other"] }
+    occupation {PATIENT_OCCUPATIONS.sample }
   end
 end
