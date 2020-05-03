81a920f52d3986eee43668a4175feb357976e80d
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-03 12:35:12 +0200

    require devise for spec

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ClinicsController, type: :controller do
+  before { sign_in create(:user) }
 
   describe "GET #index" do
-    pending "returns http success" do
+    it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end
