0853b762d30bf3b29e8ba070037418b296c0c69d
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-05 15:02:08 +0200

    Started on implementation

--- app/controllers/provider_enrollments_controller.rb
+++ app/controllers/provider_enrollments_controller.rb
@@ -1,4 +1,11 @@
 class ProviderEnrollmentsController < ApplicationController
+  layout "clinic_management"
+
+  def index
+    @provider_enrollments = ProviderEnrollment.all
+    @provider_enrollment = ProviderEnrollment.new
+  end
+
   def new
   end
 end
