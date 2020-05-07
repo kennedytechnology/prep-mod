f38bf00221d8dc79688944a061a777e9c2cf5ac7
Goran-Arsov <arsphy@yahoo.com>
2020-05-07 15:31:48 +0200

    Basic structure for provider_enrollments.

--- app/controllers/provider_enrollments_controller.rb
+++ app/controllers/provider_enrollments_controller.rb
@@ -7,5 +7,40 @@ class ProviderEnrollmentsController < ApplicationController
   end
 
   def new
+    @provider_enrollment = SupplyInventory.new
   end
+
+  def create
+    @provider_enrollment = SupplyInventory.new(provider_enrollment_params)
+
+    if @provider_enrollment.save
+      redirect_to provider_enrollments_path#(clinic_id: @supply_inventory.clinic), notice: "Successfully Added Inventory Item"
+    else
+      redirect_back fallback_location: provider_enrollments_path#(clinic_id: @supply_inventory.clinic), alert: "Error!"
+    end
+  end
+
+  def edit
+    @provider_enrollment = ProviderEnrollment.find(params[:id])
+  end
+
+  def update
+    @provider_enrollment = ProviderEnrollment.find(params[:id])
+
+    if @provider_enrollment.update(provider_enrollment_params)
+      redirect_to provider_enrollments_path#(clinic_id: @supply_inventory.clinic), notice: "Successfully Updated Inventory Item"
+    else
+      redirect_back fallback_location: provider_enrollments_path#(clinic_id: @supply_inventory.clinic), alert: "Error!"
+    end
+  end
+
+  private
+
+    def provider_enrollment_params
+      params.require(:provider_enrollment).permit(
+        :first_name, :last_name, :manufacturer, :address, :phone_number, :fax_number, :email, :npi_number,
+        :license_number, :license_type, :medial_specialty
+      )
+    end
+
 end
