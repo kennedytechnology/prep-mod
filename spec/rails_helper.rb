dc7f082a3e4003da1af1e086009cc6a37d4a5543
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-07 14:37:57 +0200

    Specs for states_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,7 @@ RSpec.configure do |config|
   # For Devise > 4.1.1
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, :type => :helper
   # Use the following instead if you are on Devise <= 4.1.1
   # config.include Devise::TestHelpers, :type => :controller
 end
