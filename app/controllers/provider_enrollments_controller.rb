754825a15f236a3b87dfeb45e7e09b9a60493adc
Goran-Arsov <arsphy@yahoo.com>
2020-05-12 20:22:47 +0200

    Permit practice_type in provider_enrollments_controller.rb

--- app/controllers/provider_enrollments_controller.rb
+++ app/controllers/provider_enrollments_controller.rb
@@ -38,9 +38,11 @@ class ProviderEnrollmentsController < ApplicationController
 
     def provider_enrollment_params
       params.require(:provider_enrollment).permit(
-        :first_name, :last_name, :practice_address, :practice_office_phone, :practice_fax_number, :practice_email, :npi_number,
-        :license_number, :license_type, :medical_specialty, :professional_license, 
-        :license_date_of_issue, :license_state_of_issue, :clinic_service_ids => [],         
+        :first_name, :last_name, :practice_address, :practice_office_phone, 
+        :practice_fax_number, :practice_email, :npi_number,:license_number, 
+        :license_type, :medical_specialty, :professional_license, 
+        :license_date_of_issue, :license_state_of_issue, :practice_type, 
+        :clinic_service_ids => [],         
       )
     end
 
