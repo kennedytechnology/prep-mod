2c538ca3292c03e6048e6f7fc6491aa86f53ee43
Goran-Arsov <arsphy@yahoo.com>
2020-05-12 21:32:10 +0200

    Permit all params in provider_enrollments_controller.rb. Add more entry boxes (in progress).

--- app/controllers/provider_enrollments_controller.rb
+++ app/controllers/provider_enrollments_controller.rb
@@ -38,11 +38,17 @@ class ProviderEnrollmentsController < ApplicationController
 
     def provider_enrollment_params
       params.require(:provider_enrollment).permit(
-        :first_name, :last_name, :practice_address, :practice_office_phone, 
-        :practice_fax_number, :practice_email, :npi_number,:license_number, 
-        :license_type, :medical_specialty, :professional_license, 
-        :license_date_of_issue, :license_state_of_issue, :practice_type, 
-        :clinic_service_ids => [],         
+        :first_name, :last_name, :middle_initial, 
+        :practice_address, :practice_office_phone, :practice_mobile_phone, 
+        :practice_fax_number, :practice_email, :title, 
+        :npi_number, :license_number, :license_type, 
+        :medical_specialty, :professional_license, :license_date_of_issue, 
+        :license_state_of_issue, :practice_type, :practice_name, 
+        :practice_city, :practice_state, :practice_zip_code, 
+        :practice_backup_contact, :practice_backup_phone, :practice_backup_email, 
+        :population_served, :high_risk_group_served, :does_provide_vaccination, 
+        :does_provide_vfc, :refrigerator, :refrigerator_thermometer, 
+        :additional_info, :clinic_service_ids => [],         
       )
     end
 
