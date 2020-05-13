8e4f845e970aa4dd5f03cde1947efb4ac434e557
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-13 19:16:56 +0200

    Fixed tests for news signup

--- spec/controllers/news_signups_controller_spec.rb
+++ spec/controllers/news_signups_controller_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe NewsSignupsController, type: :controller do
       it "create new signup" do
         expect {
           post :create, params: { news_signup: {
-            name: Faker::Name.name,
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
             email: Faker::Internet.email,
             date_of_birth: Faker::Date.birthday,
             zip_code: Faker::Address.zip_code,
@@ -36,7 +37,7 @@ RSpec.describe NewsSignupsController, type: :controller do
         occupation: PATIENT_OCCUPATIONS.sample
       }
     }
-    should permit(:name, :email, :date_of_birth, :zip_code, :occupation, topics: []).
+    should permit(:first_name, :last_name, :email, :date_of_birth, :zip_code, :occupation, topics: []).
       for(:create, params: params).
       on(:news_signup)
   end
--- spec/views/news_signups/new.html.erb_spec.rb
+++ spec/views/news_signups/new.html.erb_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "news_signups/new", type: :view do
   before(:each) do
     assign(:news_signup, NewsSignup.new(
-      name: Faker::Name.name,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
       email: Faker::Internet.email,
       date_of_birth: Faker::Date.birthday,
       zip_code: Faker::Address.zip_code,
@@ -16,7 +17,8 @@ RSpec.describe "news_signups/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", news_signups_path, "post" do
-      assert_select "input[name=?]", "news_signup[name]"
+      assert_select "input[name=?]", "news_signup[first_name]"
+      assert_select "input[name=?]", "news_signup[last_name]"
       assert_select "input[name=?]", "news_signup[email]"
       assert_select "input[name=?]", "news_signup[date_of_birth]"
       assert_select "input[name=?]", "news_signup[zip_code]"
