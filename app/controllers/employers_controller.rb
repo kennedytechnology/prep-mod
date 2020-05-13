7a52657990935354c9ca29eee19944ea95abab44
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-13 13:14:26 +0200

    Permit params + add website field

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -22,7 +22,15 @@ class EmployersController < InheritedResources::Base
   private
 
     def employer_params
-      params.require(:employer).permit()
+      params.require(:employer).permit(
+        :screening_info, :testing_info, :vacination_info, :other_info,
+        :business_locations, :first_name, :last_name, :position, :company_name,
+        :address_1, :address_2, :city, :zip_code, :state, :office_phone, :mobile_phone,
+        :email, :website, :backup_first_name, :backup_last_name, :backup_position,
+        :backup_company_name, :backup_address_1, :backup_address_2, :backup_city,
+        :backup_zip_code, :backup_state, :total_employees, :total_locations,
+        business_locations: []
+      )
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_104916) do
+ActiveRecord::Schema.define(version: 2020_05_13_111341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_104916) do
     t.string "backup_office_phone"
     t.string "backup_mobile_phone"
     t.string "backup_email"
+    t.string "website"
   end
 
   create_table "employers_patients", id: false, force: :cascade do |t|
