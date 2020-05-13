7d82888eecf817375ef38859d0f04ea862812e31
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-13 19:18:10 +0200

    Fixed test for landing page

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "Pages", type: :request do
 
   describe "GET /landing_page" do
+    let(:clinic) { create(:clinic) }
     before { sign_in create(:user) }
 
     it "returns http success" do
-      get "/pages/landing_page"
+      get "/pages/landing_page", params: {clinic_id: clinic.id}
       expect(response).to have_http_status(:success)
     end
   end
