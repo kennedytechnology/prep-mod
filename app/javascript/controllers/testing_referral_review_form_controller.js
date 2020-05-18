3b5a571e00e0f91aff501e80d5f2f7015f17b858
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-18 14:55:09 +0200

    Review form for Testing Referral

---
+++ app/javascript/controllers/testing_referral_review_form_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["modal"]
+
+  connect() {
+
+  }
+
+  toggleModal(event) {
+    let modal = document.getElementById("testingReferralModal");
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+  }
+
+  updateModalField(e) {
+    let currentFieldId = e.currentTarget.id,
+      currentFieldValue = e.currentTarget.value;
+
+    document.getElementById("modal_" + currentFieldId).value = currentFieldValue;
+  }
+
+  updateModalCheckboxField(e) {
+    let currentFieldId = e.currentTarget.id;
+    
+    if (e.currentTarget.checked) {
+      document.getElementById("modal_" + currentFieldId).checked = true;
+    } else {
+      document.getElementById("modal_" + currentFieldId).checked = false;
+    }
+  }
+}
