85745517bf996427995d26447008ce0e8c6d798b
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-20 22:16:21 +0200

    Add search field for company name of employers

---
+++ app/javascript/controllers/consent_form_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "searchableRow" ]
+
+  search(event) {
+    let searchTerm = event.target.value.toLowerCase()
+    this.searchableRowTargets.forEach((el, i) => {
+      let filterableKey =  el.getAttribute("data-search-content")
+      el.classList.toggle("hidden", !filterableKey.includes( searchTerm ) )
+    })
+  }
+}
