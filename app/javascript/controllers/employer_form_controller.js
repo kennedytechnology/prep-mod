fc48125b50deae013d4bdc6db1ccf543dc151e45
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-21 09:26:55 +0200

    Close dropdown if user clicks outside of input and checkboxes

--- app/javascript/controllers/employer_form_controller.js
+++ app/javascript/controllers/employer_form_controller.js
@@ -4,7 +4,11 @@ export default class extends Controller {
   static targets = ["locationsCheckboxes"]
 
   connect() {
-
+    $(document).click(function(e) {
+      if (e.target.id != 'predict_company_name' && !$('#locationsCheckboxes').find(e.target).length) {
+          $("#locationsCheckboxes").removeClass("active");
+      }
+    });
   }
 
   showCheckboxes() {
