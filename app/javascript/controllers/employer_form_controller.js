c8ffeda541c22a0fa1419a2cb1bb19dd87358ffa
Hristina <hristina.atanasova6@gmail.com>
2020-05-22 12:30:53 +0200

    Code refactoring

--- app/javascript/controllers/employer_form_controller.js
+++ app/javascript/controllers/employer_form_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
     if (!isValid) {
       event.preventDefault();
     }
-  } 
+  }
 
   validateForm() {
     let isValid = true;
@@ -43,7 +43,7 @@ export default class extends Controller {
     return isValid;
   }
 
-  validateFirstName(event) {
+  validateInputField(event) {
     let errorMessage = event.target.parentNode.getElementsByClassName(
       "error-message"
     )[0];
@@ -51,190 +51,64 @@ export default class extends Controller {
     if (event.target.value == "") {
       event.target.style.marginBottom = 0;
       errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "First name is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
 
-  validateLastName(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Last name is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validatePosition(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Position/title is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateAddress1(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Address is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateAddress2(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Unit number/address required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateCity(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "City is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateState(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "State is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateZipCode(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Zip Code is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateOfficePhone(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Office phone is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateMobilePhone(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Mobile phone is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateEmail(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Email address is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateEmployeesNumber(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Total number of employees is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateLocationsNumber(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Total number of locations is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
+      if (
+        event.target.name == "employer[first_name]" ||
+        event.target.name == "employer[backup_first_name]"
+      ) {
+        errorMessage.innerText = "First name is required!";
+      } else if (
+        event.target.name == "employer[last_name]" ||
+        event.target.name == "employer[backup_last_name]"
+      ) {
+        errorMessage.innerText = "Last name is required!";
+      } else if (
+        event.target.name == "employer[position]" ||
+        event.target.name == "employer[backup_position]"
+      ) {
+        errorMessage.innerText = "Position/title is required!";
+      } else if (
+        event.target.name == "employer[address_1]" ||
+        event.target.name == "employer[backup_address_1]"
+      ) {
+        errorMessage.innerText = "Address is required!";
+      } else if (
+        event.target.name == "employer[address_2]" ||
+        event.target.name == "employer[backup_address_2]"
+      ) {
+        errorMessage.innerText = "Unit number/address 2 is required!";
+      } else if (
+        event.target.name == "employer[city]" ||
+        event.target.name == "employer[backup_city]"
+      ) {
+        errorMessage.innerText = "City is required!";
+      } else if (
+        event.target.name == "employer[state]" ||
+        event.target.name == "employer[backup_state]"
+      ) {
+        errorMessage.innerText = "State is required!";
+      } else if (
+        event.target.name == "employer[zip_code]" ||
+        event.target.name == "employer[backup_zip_code]"
+      ) {
+        errorMessage.innerText = "Zip Code is required!";
+      } else if (event.target.name == "employer[office_phone]") {
+        errorMessage.innerText = "Office phone is required!";
+      } else if (event.target.name == "employer[mobile_phone]") {
+        errorMessage.innerText = "Mobile phone is required!";
+      } else if (
+        event.target.name == "employer[email]" ||
+        event.target.name == "employer[backup_email]"
+      ) {
+        errorMessage.innerText = "Email address is required!";
+      } else if (event.target.name == "employer[total_employees]") {
+        errorMessage.innerText = "Total number of employees is required!";
+      } else if (event.target.name == "employer[total_locations]") {
+        errorMessage.innerText = "Total number of locations is required!";
+      } else {
+        event.target.classList.remove("invalid");
+        errorMessage.classList.add("hidden");
+      }
     }
   }
 }
