49bdf3f6d665ba45b0b59eef03dacd6bdea5c8b8
Hristina <hristina.atanasova6@gmail.com>
2020-05-22 11:09:00 +0200

    Code improvements

--- app/javascript/controllers/employer_form_controller.js
+++ app/javascript/controllers/employer_form_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
     if (!isValid) {
       event.preventDefault();
     }
-  }
+  } 
 
   validateForm() {
     let isValid = true;
@@ -208,173 +208,6 @@ export default class extends Controller {
     }
   }
 
-  // Validate Backup Contact Inputs
-  validateBackupFirstName(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "First name is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupLastName(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-    
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Last name is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupPosition(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-    
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Position/title is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupAddress1(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Address is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupAddress2(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText =
-        "Unit number/address required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupCity(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "City is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupState(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "State is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupZipCode(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Zip Code is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupOfficePhone(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Office phone is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupMobilePhone(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Mobile phone is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
-  validateBackupEmail(event) {
-    let errorMessage = event.target.parentNode.getElementsByClassName(
-      "error-message"
-    )[0];
-
-    if (event.target.value == "") {
-      event.target.style.marginBottom = 0;
-      errorMessage.classList.remove("hidden");
-      errorMessage.innerText = "Email address is required!";
-    } else {
-      event.target.classList.remove("invalid");
-      errorMessage.classList.add("hidden");
-    }
-  }
-
   validateEmployeesNumber(event) {
     let errorMessage = event.target.parentNode.getElementsByClassName(
       "error-message"
@@ -383,8 +216,7 @@ export default class extends Controller {
     if (event.target.value == "") {
       event.target.style.marginBottom = 0;
       errorMessage.classList.remove("hidden");
-      errorMessage.innerText =
-        "Total number of employees is required!";
+      errorMessage.innerText = "Total number of employees is required!";
     } else {
       event.target.classList.remove("invalid");
       errorMessage.classList.add("hidden");
@@ -399,8 +231,7 @@ export default class extends Controller {
     if (event.target.value == "") {
       event.target.style.marginBottom = 0;
       errorMessage.classList.remove("hidden");
-      errorMessage.innerText =
-        "Total number of locations is required!";
+      errorMessage.innerText = "Total number of locations is required!";
     } else {
       event.target.classList.remove("invalid");
       errorMessage.classList.add("hidden");
