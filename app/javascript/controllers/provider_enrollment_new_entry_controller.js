58f41283d20f7caf1c55be2bd93d7e39ca07600d
Goran-Arsov <arsphy@yahoo.com>
2020-05-22 12:56:51 +0200

    Disable text_field when check_box is checked. Enable and clear when unchecked

--- app/javascript/controllers/provider_enrollment_new_entry_controller.js
+++ app/javascript/controllers/provider_enrollment_new_entry_controller.js
@@ -14,14 +14,26 @@ export default class extends Controller {
     if (e.currentTarget.id == "provider_enrollment_refrigerator") {
       if (e.currentTarget.checked) {
         this.refrigerator_textTarget.value = "I do not have a refrigerator"
+        $(this.refrigerator_textTarget).attr("disabled", true)
+      } else {
+        this.refrigerator_textTarget.value = ""
+        $(this.refrigerator_textTarget).removeAttr("disabled")
       }
     } else if (e.currentTarget.id == "provider_enrollment_refrigerator_thermometer") {
       if (e.currentTarget.checked) {
         this.refrigerator_thermometer_textTarget.value = "I do not have a refrigerator thermometer"
+        $(this.refrigerator_thermometer_textTarget).attr("disabled", true)
+      } else {
+        this.refrigerator_thermometer_textTarget.value = ""
+        $(this.refrigerator_thermometer_textTarget).removeAttr("disabled")
       }
     } else {
       if (e.currentTarget.checked) {
         this.freezer_textTarget.value = "I do not have a freezer"
+        $(this.freezer_textTarget).attr("disabled", true)
+      } else {
+        this.freezer_textTarget.value = ""
+        $(this.freezer_textTarget).removeAttr("disabled")
       }
     }
   }
