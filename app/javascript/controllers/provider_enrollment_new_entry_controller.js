4759422e46651f88afdc76c85c379a6cf8718bb2
Goran-Arsov <arsphy@yahoo.com>
2020-05-22 12:07:31 +0200

    Use stimulus to create check_box input into text_field functionality for new provider enrollment

--- app/javascript/controllers/provider_enrollment_new_entry_controller.js
+++ app/javascript/controllers/provider_enrollment_new_entry_controller.js
@@ -3,17 +3,27 @@ import {
 } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["refrigerator", "refrigerator_text",
+    "refrigerator_thermometer", "refrigerator_thermometer_text",
+    "freezer", "freezer_text"
+  ]
 
-  connect() {
+  connect() {}
 
+  fillTextField(e) {
+    if (e.currentTarget.id == "provider_enrollment_refrigerator") {
+      if (e.currentTarget.checked) {
+        this.refrigerator_textTarget.value = "I do not have a refrigerator"
+      }
+    } else if (e.currentTarget.id == "provider_enrollment_refrigerator_thermometer") {
+      if (e.currentTarget.checked) {
+        this.refrigerator_thermometer_textTarget.value = "I do not have a refrigerator thermometer"
+      }
+    } else {
+      if (e.currentTarget.checked) {
+        this.freezer_textTarget.value = "I do not have a freezer"
+      }
+    }
   }
 
-  updateField() {
-    // testing if this works
-    refrigerator = document.getElementById("refrigerator");
-    refrigerator.querySelector("input").value = "No!!!";
-    // textInput = refrigerator.querySelector("input[type=text]");
-    // textInput.value = "No";
-  }
 }
