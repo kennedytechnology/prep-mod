70cb874717f874208d4dcdffc79db9b2116ec767
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-26 12:37:54 +0200

    Calculations for quantity

--- app/models/supply_inventory.rb
+++ app/models/supply_inventory.rb
@@ -1,3 +1,19 @@
 class SupplyInventory < ApplicationRecord
   has_many :supply_inventory_events
+
+  def quantity_lost_sum
+    supply_inventory_events.sum(:quantity_lost)
+  end
+
+  def quantity_used_sum
+    supply_inventory_events.sum(:quantity_used)
+  end
+
+  def quantity_loaned_sum
+    supply_inventory_events.sum(:quantity_loaned)
+  end
+
+  def remaining_quantity_sum
+    quantity - (quantity_lost_sum + quantity_used_sum + quantity_loaned_sum)
+  end
 end
