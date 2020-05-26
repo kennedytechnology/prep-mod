fc486f149b4b92cf73d464d1bffa8cd0e1d2a1ad
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-05-26 12:23:55 +0200

    Specs fix

--- spec/controllers/supply_inventories_controller_spec.rb
+++ spec/controllers/supply_inventories_controller_spec.rb
@@ -23,28 +23,12 @@ RSpec.describe SupplyInventoriesController, type: :controller do
             lot_number: Faker::Code.asin,
             expiration_date: Faker::Date.between(from: Date.today, to: 30.days.from_now),
             quantity: Faker::Number.between(from: 10, to: 20),
-            quantity_used: Faker::Number.between(from: 1, to: 5),
-            quantity_lost: Faker::Number.between(from: 1, to: 3),
-            quantity_loaned: Faker::Number.between(from: 1, to: 3),
             packaging: INVENTORY_PACKAGINGS.sample,
             source: INVENTORY_SOURCES.sample,
             product_name: Faker::Company.name,
-            event_type: INVENTORY_EVENT_TYPES.sample
           } }
         }.to change(SupplyInventory, :count).by(1)
       end
-    end
-  end
-
-  describe "POST #update" do
-    context "success" do
-      before do
-        put :update, params: { id: supply_inventory.id, supply_inventory: { received_at: Date.today } }
-        supply_inventory.reload
-      end
-
-      it { expect(response).to redirect_to supply_inventories_path }
-      it { expect(supply_inventory.received_at).to eq(Date.today) }
     end
   end
 
@@ -58,20 +42,14 @@ RSpec.describe SupplyInventoriesController, type: :controller do
         lot_number: Faker::Code.asin,
         expiration_date: Faker::Date.between(from: Date.today, to: 30.days.from_now),
         quantity: Faker::Number.between(from: 10, to: 20),
-        quantity_used: Faker::Number.between(from: 1, to: 5),
-        quantity_lost: Faker::Number.between(from: 1, to: 3),
-        quantity_loaned: Faker::Number.between(from: 1, to: 3),
         packaging: INVENTORY_PACKAGINGS.sample,
         source: INVENTORY_SOURCES.sample,
-        product_name: Faker::Company.name,
-        event_type: INVENTORY_EVENT_TYPES.sample
+        product_name: Faker::Company.name
       }
     }
     should permit(:received_at, :item_type, :item_name, 
         :manufacturer, :lot_number, :expiration_date,
-        :quantity, :quantity_used, :quantity_loaned,
-        :quantity_lost, :packaging, :source, :product_name, 
-        :event_type).
+        :quantity, :packaging, :source, :product_name).
       for(:create, params: params).
       on(:supply_inventory)
   end
