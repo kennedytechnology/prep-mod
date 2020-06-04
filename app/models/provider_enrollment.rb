a2849fab7a171279aa153c758e5ac1083263ea21
Goran-Arsov <arsphy@yahoo.com>
2020-06-04 12:50:00 +0200

    Describe a method with a better name

--- app/models/provider_enrollment.rb
+++ app/models/provider_enrollment.rb
@@ -7,10 +7,10 @@ class ProviderEnrollment < ApplicationRecord
   accepts_nested_attributes_for :clinic_services
   has_many :clinics
 
-  after_create :default_status_and_unique_number, if: lambda { unique_number == nil }
+  after_create :set_default_status_and_unique_number, if: lambda { unique_number == nil }
   after_update :send_email, if: lambda { status == "accepted" }
 
-  def default_status_and_unique_number
+  def set_default_status_and_unique_number
     self.status = "pending"
     self.unique_number = "P#{self.id}"
     self.save
