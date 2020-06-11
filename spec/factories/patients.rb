477587868f21fbc08a60d71c8ee9600948507114
Goran-Arsov <arsphy@yahoo.com>
2020-06-11 11:44:31 +0200

    Fix failing tests

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :patient do
     # association :clinic, factory: :clinic
+    association :appointment, factory: :appointment
 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
