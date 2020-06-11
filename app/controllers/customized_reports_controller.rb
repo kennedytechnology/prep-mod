6ee8afe1b9cb3da6275587e7bb13845093a420aa
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-06-11 09:06:48 +0200

    Enable finding patients by county

--- app/controllers/customized_reports_controller.rb
+++ app/controllers/customized_reports_controller.rb
@@ -26,11 +26,12 @@ class CustomizedReportsController < InheritedResources::Base
 
   def create
     # @customized_report = CustomizedReport.new(customized_report_params)
-    @clinic = Clinic.find(customized_report_params[:clinic_id])
+    @clinic = Clinic.find(customized_report_params[:clinic_id]) if customized_report_params[:clinic_id]
 
     @patients_screened = Patient.limit(5) if customized_report_params[:patient_screened]
     @patients_tested = Patient.limit(5) if customized_report_params[:patient_tested]
     @patients_safety_kit = Patient.limit(5) if customized_report_params[:safety_kit]
+    @patients_by_county = Patient.where(county: customized_report_params[:county]) if customized_report_params[:county]
 
     @age = true if customized_report_params[:age]
     @gender = true if customized_report_params[:gender]
@@ -39,10 +40,11 @@ class CustomizedReportsController < InheritedResources::Base
     @insurance_type = true if customized_report_params[:insurance_type]
     @date_of_birth = true if customized_report_params[:date_of_birth]
     @vacine_name_and_lot_number = true if customized_report_params[:vacine_name_and_lot_number]
+    @county = true if @patients_by_county
 
     respond_to do |format|
       format.pdf do
-        render pdf: "Customized report for #{@clinic.name}",
+        render pdf: "Customized report",
                 page_size: 'A4',
                 template: "customized_reports/report_pdf.html.erb",
                 layout: "clinic-print.pdf.erb",
@@ -63,6 +65,7 @@ class CustomizedReportsController < InheritedResources::Base
         :patient_screened, :patient_tested, :safety_kit, 
         :age, :gender, :race, :clinic_id, :zip_code,
         :insurance_type, :date_of_birth, :vacine_name_and_lot_number,
+        :county
       )
     end
 
