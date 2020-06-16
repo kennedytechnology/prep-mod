66de79af9a3650cc1733e9b9f65e8491dff2736d
Hristina <hristina.atanasova6@gmail.com>
2020-06-16 12:36:15 +0200

    Preview email and add full_name method

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,4 +1,8 @@
 class Employer < ApplicationRecord
   serialize :business_locations, Array
   has_and_belongs_to_many :patients
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/mailers/previews/employer_mailer_preview.rb
+++ spec/mailers/previews/employer_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/employer_mailer
 class EmployerMailerPreview < ActionMailer::Preview
 
+  # Preview this email at http://localhost:3000/rails/mailers/employer/request_confirmation
+  def request_confirmation
+    EmployerMailer.request_confirmation(Employer.last)
+  end
+
 end
