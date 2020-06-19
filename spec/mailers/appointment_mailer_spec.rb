bc05db67823be8589480398c8491aaa1aa727fb0
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-06-19 16:07:37 +0200

    Add specs for mailer

--- spec/mailers/appointment_mailer_spec.rb
+++ spec/mailers/appointment_mailer_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe AppointmentMailer, type: :mailer do
       expect(mail.body.encoded).to match("We will text and/or email you when we're ready for you.")
     end
   end
+
+  describe "send_reminder" do
+    let(:appointment) {create(:appointment)}
+    let(:mail) { AppointmentMailer.send_reminder(appointment) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Upcoming appointment reminder")
+      expect(mail.to).to eq([appointment.patient.email])
+      expect(mail.from).to eq(["notifications@clinicwizard.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("#{ENV['SITE_URL']}/check_in/#{appointment.patient.check_in_code}")
+    end
+  end
 end
--- spec/mailers/previews/appointment_mailer_preview.rb
+++ spec/mailers/previews/appointment_mailer_preview.rb
@@ -1,6 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/appointment_mailer
 class AppointmentMailerPreview < ActionMailer::Preview
-  def request_confirmation
+  def patient_checked_in
     AppointmentMailer.patient_checked_in(Appointment.first)
+  end
+
+  def send_reminder
+    AppointmentMailer.send_reminder(Appointment.first)
   end
 end
