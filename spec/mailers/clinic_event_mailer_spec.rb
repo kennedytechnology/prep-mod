31b1c09bb01473ab25cf1b2dce122d4407be1051
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-06-22 12:56:17 +0200

    Add specs

--- spec/mailers/clinic_event_mailer_spec.rb
+++ spec/mailers/clinic_event_mailer_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe ClinicEventMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "registration_confirmed" do
+    let(:clinic_event) { create(:clinic_event) }
+    let(:mail) { ClinicEventMailer.registration_confirmed(clinic_event) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("You are registered for the clinic on #{clinic_event.event_date.strftime("%d/%m/%Y")}")
+      expect(mail.to).to eq([clinic_event.patient.email])
+      expect(mail.from).to eq(["notifications@clinicwizard.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("#{clinic_event.event_date.strftime("%d/%m/%Y")}")
+    end
+  end
 end
--- spec/mailers/previews/clinic_event_mailer_preview.rb
+++ spec/mailers/previews/clinic_event_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/clinic_event_mailer
 class ClinicEventMailerPreview < ActionMailer::Preview
-
+  def patient_checked_in
+    ClinicEventMailer.registration_confirmed(ClinicEvent.first)
+  end
 end
