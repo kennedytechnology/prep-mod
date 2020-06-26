0965ed7dd211b77b27630e47caec6474b79d2144
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-06-26 09:10:44 +0200

    clinic event factory fix

--- spec/factories/clinic_events.rb
+++ spec/factories/clinic_events.rb
@@ -4,11 +4,9 @@ FactoryBot.define do
     association :patient, factory: :patient
     association :user, factory: :user
 
-    clinic_event = CLINIC_EVENTS.sample
-
-    category { clinic_event[:name] } 
+    category { CLINIC_EVENTS.first[:name] } 
     clinic_staff_id { clinic.clinic_personnel.sample }
-    outcome { clinic_event[:outcome] }
+    outcome {  CLINIC_EVENTS.first[:outcomes].sample }
     created_at { Faker::Date.between(from: 30.days.ago, to: Date.today) }
     event_date { Faker::Date.between(from: Date.today, to: 1.year.from_now) }
     location { Faker::Address.street_address }
