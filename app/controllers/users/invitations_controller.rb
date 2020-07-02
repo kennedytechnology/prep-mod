f1b9aed6f7a98bcd2359bf1ecbde1d87b6051a24
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-07-02 10:01:30 +0200

    Create/Invite new user fixes

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -5,6 +5,6 @@ class Users::InvitationsController < Devise::InvitationsController
   protected
 
   def configure_permitted_parameters    
-    devise_parameter_sanitizer.permit(:invite, keys: [:first_name, :last_name, :venues, :role])
+    devise_parameter_sanitizer.permit(:invite, keys: [:first_name, :last_name, :role, venues: [] ])
   end
 end
