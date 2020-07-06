f42011898cf7a480035b3636bd3840b3a40f09ea
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-07-06 13:55:30 +0200

    Confirmation message correction

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,6 +1,6 @@
 class EmployerMailer < ApplicationMailer
     def request_confirmation(employer)
         @employer = employer
-        mail to: employer.email, subject: "Confirmation for your request to access information about your workplace"
+        mail to: employer.email, subject: "Your request to receive COVID-19 information about your constituents or employees has been submitted successfully."
     end
 end
