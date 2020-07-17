50886cb1c7d58db38d4d414aad5fd918f4c41ac6
Teala <32497424+tseferi@users.noreply.github.com>
2020-07-17 17:57:48 +0200

    Registration list, number of registered patients, number of patients on waiting list. (#434)
    Co-authored-by: [tseferi] <[teala.seferi@cns.uist.edu.mk]>

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -29,6 +29,10 @@ class PatientsController < ApplicationController
       @patients = Patient.with_appointments(params[:date_of_birth]).order(:date_of_birth)
     end
 
+    if params[:clinic_id]
+      @patients_waiting_list = Clinic.find(params[:clinic_id]).patients.with_appointments.count
+      @patients_appointments = Clinic.find(params[:clinic_id]).appointments_count
+    end
   end
 
   def anonymized_index
