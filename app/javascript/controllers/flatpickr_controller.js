1eb963837a02fba7e3406cef27d8b77296ff0bc6
OrceMaceski <40167474+OrceMaceski@users.noreply.github.com>
2020-07-21 14:19:21 +0200

    Flatpickr value fixes (#449)
    * Fixed flatpickr value for patient search function
    
    * Fixed flatpickr value for test_kits

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,7 +5,7 @@ import '../css/calendar.css'
 export default class extends Flatpickr {
     initialize() {
         this.config = {
-            defaultDate: this.data.get('default-date'),
+            // defaultDate: this.data.get('default-date'),
             altInput: true,
             altFormat: "F j, Y"
         }
