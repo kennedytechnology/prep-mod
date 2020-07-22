f86d6ea16a2f6acd18f99f975e40a8449263db2c
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-07-22 13:09:45 +0200

    Load form only where needed

---
+++ app/javascript/packs/prevent_unsaved_form.js
@@ -0,0 +1,13 @@
+// Prevent leaving page if form is unsaved
+// // $(document).on('turbolinks:load', function () {
+  $(function () {
+      $("input, textarea, select").on("input change", function() {
+          window.onbeforeunload = window.onbeforeunload || function (e) {
+              return "You have unsaved changes.  Do you want to leave this page and lose your changes?";
+          };
+      });
+      $("form").on("submit", function() {
+          window.onbeforeunload = null;
+      });
+  })
+// // });
