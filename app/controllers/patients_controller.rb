2bf82fe498baff7de82e2803509b3a5f3d218c44
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-07-24 11:18:42 +0200

    Download list of patients on waiting list for a clinic fix

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -14,7 +14,7 @@ class PatientsController < ApplicationController
       format.html
       format.csv { send_data @clinic.patients.to_csv, filename: "news_signups-#{Date.today}.csv" }
       format.xlsx do
-        @patients = Patient.on_waiting_list
+        @patients = @clinic.patients.on_waiting_list
         render  template: 'patients/index',
                 disposition: 'inline',
                 xlsx: "patients_waiting_list_#{Date.today.strftime("%d_%m_%Y")}.xlsx",
