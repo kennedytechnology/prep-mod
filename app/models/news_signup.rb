3f566103ce2618168705a358c1b86954cc56152c
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-07-27 11:32:44 +0200

    Make topics required

--- app/models/news_signup.rb
+++ app/models/news_signup.rb
@@ -3,4 +3,9 @@ class NewsSignup < ApplicationRecord
 
   validates_presence_of :email, :first_name, :last_name, :date_of_birth, :zip_code, :occupation
   validates :chronic_health_condition, inclusion: { in: [ true, false ] }
+  validate :topics_cannot_be_empty
+
+  def topics_cannot_be_empty
+    errors.add(:base, "You must select at least 1 topic.") if topics.any?{ |t| t == "0" }
+  end
 end
