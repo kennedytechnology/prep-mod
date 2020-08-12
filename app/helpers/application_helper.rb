a4c4c8eb3d14bacf86424fc44bbd0d66a9d23afb
OrceMaceski <40167474+OrceMaceski@users.noreply.github.com>
2020-08-12 15:03:43 +0200

    Flatpickr module (#548)
    * Flatpickr module with environment variable
    
    * Custom form tag for flatpickr
    
    * Added custom form tag to provider enrolments
    
    * Added custom form field tag datepicker for the rest of the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 module ApplicationHelper
+
+  def cw_date_field(form, name, options = {}, args = {})
+    if args["data-flatpickr-min-date"]
+      date_options = {order: [:month, :day, :year], start_year: Date.current.year - 99, end_year: Date.current.year, include_blank: true}
+    elsif args["data-flatpickr-max-date"]
+      date_options = {order: [:month, :day, :year], start_year: Date.current.year, end_year: Date.current.year + 10, include_blank: true}
+    else
+      date_options = {order: [:month, :day, :year], include_blank: true}
+    end
+
+    ENV['DATEPICKER'] == "true" ? form.date_select(name, date_options, class: "form-select") : form.text_field(name, options.merge!(args))
+  end
+
   def school_mode?
     return (ENV["SCHOOL_VACCINATION_MODE"] && (ENV["SCHOOL_VACCINATION_MODE"] == "true")) ? true : false
   end
+
 end
