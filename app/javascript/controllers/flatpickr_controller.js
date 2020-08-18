651f36be4bc4774a998a48d1e61f4efbae94f16d
OrceMaceski <40167474+OrceMaceski@users.noreply.github.com>
2020-08-18 12:31:10 +0200

    Flatpickr year dropdown menu (#604)
    * Added dropdown ability to flatpickr
    
    * Fixed end year display and background color to year dropdown menu
    
    * Add padding top
    
    Co-authored-by: Violeta P <32481414+violeta-p@users.noreply.github.com>

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,6 +1,7 @@
 import Flatpickr from 'stimulus-flatpickr'
 
 import '../css/calendar.css'
+const FLATPICKR_CUSTOM_YEAR_SELECT = 'flatpickr-custom-year-select';
 
 export default class extends Flatpickr {
     initialize() {
@@ -8,12 +9,49 @@ export default class extends Flatpickr {
             altInput: true,
             altFormat: "F j, Y",
             allowInput: true,
+            minDate: "01.01.1920",
+            maxDate: "31.12.2049",
+
             onOpen: function(selectedDates, dateStr, instance) {
                 $(instance.altInput).prop('readonly', true);
             },
             onClose: function(selectedDates, dateStr, instance) {
                 $(instance.altInput).prop('readonly', false);
                 $(instance.altInput).blur();
+            },
+            onReady: function (selectedDates, dateStr, instance) {
+                const flatpickrYearElement = instance.currentYearElement;
+
+                const children = flatpickrYearElement.parentElement.children;
+                for (let i in children) {
+                    if (children.hasOwnProperty(i)) {
+                        children[i].style.display = 'none';
+                    }
+                }
+
+                const yearSelect = document.createElement('select');
+                const minYear = new Date(instance.config._minDate).getFullYear();
+                const maxYear = new Date(instance.config._maxDate).getFullYear();
+                for (let i = minYear; i <= maxYear; i++) {
+                    const option = document.createElement('option');
+                    option.value = '' + i;
+                    option.text = '' + i;
+                    yearSelect.appendChild(option);
+                }
+                yearSelect.addEventListener('change', function (event) {
+                    flatpickrYearElement.value = event.target['value'];
+                    instance.currentYear = parseInt(event.target['value']);
+                    instance.redraw();
+                });
+
+                yearSelect.className = 'flatpickr-monthDropdown-months';
+                yearSelect.id = FLATPICKR_CUSTOM_YEAR_SELECT;
+                yearSelect.value = instance.currentYearElement.value;
+
+                flatpickrYearElement.parentElement.appendChild(yearSelect);
+            },
+            onMonthChange: function (selectedDates, dateStr, instance) {
+                document.getElementById(FLATPICKR_CUSTOM_YEAR_SELECT).value = '' + instance.currentYear;
             }
         }
     }
