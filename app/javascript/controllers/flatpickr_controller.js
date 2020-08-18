e3ec69aa0d9d7675c11daa390bc2117edc8ec375
OrceMaceski <40167474+OrceMaceski@users.noreply.github.com>
2020-08-18 17:16:44 +0200

    Reverted changes to flatpickr year dropdown menu (#613)

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,7 @@
 import Flatpickr from 'stimulus-flatpickr'
 
 import '../css/calendar.css'
-const FLATPICKR_CUSTOM_YEAR_SELECT = 'flatpickr-custom-year-select';
+// const FLATPICKR_CUSTOM_YEAR_SELECT = 'flatpickr-custom-year-select';
 
 export default class extends Flatpickr {
     initialize() {
@@ -9,8 +9,8 @@ export default class extends Flatpickr {
             altInput: true,
             altFormat: "F j, Y",
             allowInput: true,
-            minDate: "01.01.1920",
-            maxDate: "31.12.2049",
+            // minDate: "01.01.1920",
+            // maxDate: "31.12.2049",
 
             onOpen: function(selectedDates, dateStr, instance) {
                 $(instance.altInput).prop('readonly', true);
@@ -19,40 +19,40 @@ export default class extends Flatpickr {
                 $(instance.altInput).prop('readonly', false);
                 $(instance.altInput).blur();
             },
-            onReady: function (selectedDates, dateStr, instance) {
-                const flatpickrYearElement = instance.currentYearElement;
+            // onReady: function (selectedDates, dateStr, instance) {
+            //     const flatpickrYearElement = instance.currentYearElement;
 
-                const children = flatpickrYearElement.parentElement.children;
-                for (let i in children) {
-                    if (children.hasOwnProperty(i)) {
-                        children[i].style.display = 'none';
-                    }
-                }
+            //     const children = flatpickrYearElement.parentElement.children;
+            //     for (let i in children) {
+            //         if (children.hasOwnProperty(i)) {
+            //             children[i].style.display = 'none';
+            //         }
+            //     }
 
-                const yearSelect = document.createElement('select');
-                const minYear = new Date(instance.config._minDate).getFullYear();
-                const maxYear = new Date(instance.config._maxDate).getFullYear();
-                for (let i = minYear; i <= maxYear; i++) {
-                    const option = document.createElement('option');
-                    option.value = '' + i;
-                    option.text = '' + i;
-                    yearSelect.appendChild(option);
-                }
-                yearSelect.addEventListener('change', function (event) {
-                    flatpickrYearElement.value = event.target['value'];
-                    instance.currentYear = parseInt(event.target['value']);
-                    instance.redraw();
-                });
+            //     const yearSelect = document.createElement('select');
+            //     const minYear = new Date(instance.config._minDate).getFullYear();
+            //     const maxYear = new Date(instance.config._maxDate).getFullYear();
+            //     for (let i = minYear; i <= maxYear; i++) {
+            //         const option = document.createElement('option');
+            //         option.value = '' + i;
+            //         option.text = '' + i;
+            //         yearSelect.appendChild(option);
+            //     }
+            //     yearSelect.addEventListener('change', function (event) {
+            //         flatpickrYearElement.value = event.target['value'];
+            //         instance.currentYear = parseInt(event.target['value']);
+            //         instance.redraw();
+            //     });
 
-                yearSelect.className = 'flatpickr-monthDropdown-months';
-                yearSelect.id = FLATPICKR_CUSTOM_YEAR_SELECT;
-                yearSelect.value = instance.currentYearElement.value;
+            //     yearSelect.className = 'flatpickr-monthDropdown-months';
+            //     yearSelect.id = FLATPICKR_CUSTOM_YEAR_SELECT;
+            //     yearSelect.value = instance.currentYearElement.value;
 
-                flatpickrYearElement.parentElement.appendChild(yearSelect);
-            },
-            onMonthChange: function (selectedDates, dateStr, instance) {
-                document.getElementById(FLATPICKR_CUSTOM_YEAR_SELECT).value = '' + instance.currentYear;
-            }
+            //     flatpickrYearElement.parentElement.appendChild(yearSelect);
+            // },
+            // onMonthChange: function (selectedDates, dateStr, instance) {
+            //     document.getElementById(FLATPICKR_CUSTOM_YEAR_SELECT).value = '' + instance.currentYear;
+            // }
         }
     }
 }
